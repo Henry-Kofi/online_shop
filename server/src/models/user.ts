e70732d17ce49e878fbc6ae2d30970a1b844a516
Henry-Kofi <hnyonyo001@st.ug.edu.gh>
2024-05-01 22:40:23 +0000

    structure of database

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,1 +1,38 @@
-console.log("User model")
+import mongoose,{Schema,model,Document} from "mongoose";
+
+export interface User extends Document{
+    email: string;
+    phone: string;
+    password: string;
+    name: string;
+    role: string;
+    isVerified: boolean
+}
+
+const userSchema: Schema = new Schema<User>({
+    email: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    role:{
+        type: String,
+        enum:["user","vendor","rider"],
+        default: "user"
+    },
+    isVerified:{
+        type: Boolean,
+        default: false
+    }
+},{timestamps: true});
+
+const userModel = model<User>("users",userSchema);
+
+export default userModel;
