ee388d0391f6ae0130b6dba046245b5012f581ff
Henry-Kofi <hnyonyo001@st.ug.edu.gh>
2024-05-01 14:48:44 +0000

    initial set up

---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+import express,{Application} from "express"
+import mongoose from "mongoose"
+import "dotenv/config"
+import { createServer, Server } from "http";
+
+const app: Application = express();
+const server: Server = createServer(app);
+
+if (!process.env.PORT) {
+    throw new Error("PORT environment variable is not set");
+  }
+  
+  if (!process.env.MONGO_URL) {
+    throw new Error("MONGO_URL environment variable is not set");
+  }
+
+const port  = parseInt(process.env.PORT)
+const mongo_url  = String(process.env.MONGO_URL)
+app.use(express.json());
+
+mongoose.connect(mongo_url)
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((err) => console.error(`Mongoose error: ${err}`))
+
+const connection = mongoose.connection;
+
+server.listen(port,() => {
+    console.log(`Server running on port ${port}`)
+})
+
+// for debugging 
+process.on("uncaughtException",(err) => {
+    console.error(`Uncaught exception ${err}`)
+})
