ded00ee81cfadd38e1b4d03118dd611dac6b30b1
Henry-Kofi <hnyonyo001@st.ug.edu.gh>
2024-05-01 22:41:37 +0000

    endpoints for authentication(registration and verification)

--- server/src/route/user.ts
+++ server/src/route/user.ts
@@ -1,1 +1,10 @@
-console.log("User route")
+import { Router } from "express";
+import { register, verifyUser } from "../controller/user";
+
+const authRoute  = Router()
+
+authRoute.post("/register",register);
+authRoute.put("/verify",verifyUser)
+
+
+export default authRoute
